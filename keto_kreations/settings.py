f15edbdc3a1165493e397a72e890fad3072e2726
Ant <ant.griffiths22@gmail.com>
2023-12-14 01:48:28 +0000

    CHG: Update security key and debug

--- keto_kreations/settings.py
+++ keto_kreations/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jgr_f=7z22c+0h%*97fy)wa45qz-uc_sqmg-u1@!)ks_kc2lwl'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['keto-kreations-25ff0a2cbc9e.herokuapp.com', 'localhost']
 
