b3d1730e46fdc65780c13800ddee656f33458101
Ant <ant.griffiths22@gmail.com>
2023-12-23 15:29:14 +0000

    BUG: Fix anon user can't checkout
    Initialise discount code to 0 as template couldn't access unset variable

--- checkout/views.py
+++ checkout/views.py
@@ -201,6 +201,7 @@ def checkout_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    discount_amount = 0
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
